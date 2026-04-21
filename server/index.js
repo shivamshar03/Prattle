fbfe6e3f475df63fef9107bb8bc58d1d2464ceb2
shivamshar03 <shivamshar0310@gmail.com>
2026-04-21 16:23:11 +0530

    Configure repo structure for Glitch deployment

--- server/index.js
+++ server/index.js
@@ -127,5 +127,5 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 server.listen(PORT, () => console.log(`Backend server running on port ${PORT}`));
